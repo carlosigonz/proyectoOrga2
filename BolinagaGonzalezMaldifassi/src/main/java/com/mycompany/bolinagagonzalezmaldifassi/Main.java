c15fc9e25d95a2a9cdf07ac2f77e725744127cd4
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-27 13:20:19 -0400

    Creado proyecto en Java

---
+++ BolinagaGonzalezMaldifassi/src/main/java/com/mycompany/bolinagagonzalezmaldifassi/Main.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.bolinagagonzalezmaldifassi;
+
+/**
+ *
+ * @author carlo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
