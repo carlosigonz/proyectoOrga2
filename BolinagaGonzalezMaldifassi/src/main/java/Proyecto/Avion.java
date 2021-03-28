733c496d9e7fc6612c8512db98305e4523ffd838
Carlos Gonzalez <carlosigonz@gmail.com>
2021-03-27 23:02:23 -0400

    Emilinado piloto. No era necesario

--- BolinagaGonzalezMaldifassi/src/main/java/Proyecto/Avion.java
+++ BolinagaGonzalezMaldifassi/src/main/java/Proyecto/Avion.java
@@ -5,8 +5,7 @@ package Proyecto;
  * @author Nicolas Boliaga, Carlos Gonzalez, Felix Maldifassi
  */
 public class Avion {
-    String serial, modelo, nombre;
-    Piloto piloto;
+    String serial, modelo, nombre, piloto;    
     
     public Avion(String serial, String modelo, String nombre){
         this.serial = serial;
